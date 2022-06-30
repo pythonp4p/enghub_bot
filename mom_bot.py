3fbb1ba261ce4912372b107a0bf59055984b88c8
pythonp4p <102689466+pythonp4p@users.noreply.github.com>
2022-06-30 16:20:39 +0300

    Update mom_bot.py

--- mom_bot.py
+++ mom_bot.py
@@ -2,22 +2,18 @@ from email import message
 import os
 from venv import psycopg2
 from aiogram.utils import executor
-from create_bot import dp, bot, URL_APP, DB_URI
+from create_bot import dp, bot, URL_APP
 
-# Запускаем функцию старта базы данных
+
 async def on_startup(dp):
     await bot.set_webhook(url=URL_APP)
 
 async def on_shutdown(dp):
     await bot.delete_webhook()
 
-db_connection = psycopg2.connect(DB_URI, sslmode="require")
-db_object = db_connection.cursor()
 
-
-# Импортируем функции из наших файлов
 from handlers import client, other
-# Запускаем наши функции
+
 id = message.from_user.id
 username = message.from_user.username
 client.register_handlers_client(dp)
